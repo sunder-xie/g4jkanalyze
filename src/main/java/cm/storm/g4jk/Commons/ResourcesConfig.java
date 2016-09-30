228cef1e3f3d82a025f74d83ec34cd13b03f3399
chinamobile <yanxuexu007@126.com>
2016-09-30 16:43:11 +0800

    666

--- src/main/java/cm/storm/g4jk/Commons/ResourcesConfig.java
+++ src/main/java/cm/storm/g4jk/Commons/ResourcesConfig.java
@@ -13,8 +13,8 @@ public class ResourcesConfig {
 	
 	// redis集群数据库的数据库连接池配置信息，入口ip和端口
 	public final static int MAX_ACTIVE = -1;	 									//可用连接实例的最大数目，默认值为8；如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-	public final static int MAX_IDLE = 100;			 								//控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值是8。空闲代表可以复用。
-	public final static int MIN_IDLE = 10;			 								//控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值是8。空闲代表可以复用。
+	public final static int MAX_IDLE = 10;			 								//控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值是8。空闲代表可以复用。
+	public final static int MIN_IDLE = 5;			 									//控制一个pool最少有多少个状态为idle(空闲的)的jedis实例，默认值是0。空闲代表可以复用。
 	public final static int MAX_WAIT =-1;  											//等待可用连接的最大时间，单位毫秒，默认值为-1，表示等待永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
 	public final static boolean TEST_ON_BORROW = true;					//在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
 	
