451741ba94f0489c2d185e5c8ce23460886b4822
chinamobile <yanxuexu007@126.com>
2016-10-17 18:26:06 +0800

    更新为base32编解码，保证中文转成数据和英文的组合

--- src/main/java/cm/storm/g4jk/Bolts/Yunguan_G4JK_ChineseWordsCountToRedis.java
+++ src/main/java/cm/storm/g4jk/Bolts/Yunguan_G4JK_ChineseWordsCountToRedis.java
@@ -1,6 +1,5 @@
 package cm.storm.g4jk.Bolts;
 
-import java.security.MessageDigest;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +26,7 @@ public class Yunguan_G4JK_ChineseWordsCountToRedis extends BaseRichBolt {
 
 	//获取redis连接
 	private RedisServer redisserver;
-	
+
 	//初始化bolt元组搜集器，用于存放需要发射元组
 	@SuppressWarnings("rawtypes")
 	@Override
@@ -50,8 +49,8 @@ public class Yunguan_G4JK_ChineseWordsCountToRedis extends BaseRichBolt {
 			tdate=tuple.getStringByField("TupleDate");
 			chwords=tuple.getStringByField("ChineseInfo");
 			if(chwords!=null&&chwords.length()>2&&tdate!=null&&tdate.length()==10){
-				//如果获取的词的长度大于7，才进行拆词
-				if(chwords.length()>7){
+				//如果获取的词的长度大于6，才进行拆词
+				if(chwords.length()>6){
 					//1.对中文做分词，移除停用词，采用words库，详细参考pom的配置
 					words=WordSegmenter.seg(chwords);
 					//2.对热词做md5转码，然后存入集合中，同时每个字符做计数
@@ -59,8 +58,8 @@ public class Yunguan_G4JK_ChineseWordsCountToRedis extends BaseRichBolt {
 						for(int i=0;i<words.size();i++)
 						{
 							chwords=words.get(i).getText();
-							if(chwords!=null&&chwords.length()>=2)chwords=md5str32(chwords);
-							if(chwords!=null&&chwords.length()==32){
+							if(chwords!=null&&chwords.length()>=2)chwords=Base32Encode(chwords);
+							if(chwords!=null&&chwords.length()>0){
 								key="mfg4_"+tdate+"_ChineseSet";
 								redisserver.sadd(key, chwords);
 								key="mfg4_"+tdate+"_Zh_"+chwords;
@@ -70,8 +69,8 @@ public class Yunguan_G4JK_ChineseWordsCountToRedis extends BaseRichBolt {
 						}
 					}
 				}else{
-					chwords=md5str32(chwords);
-					if(chwords!=null&&chwords.length()==32){
+					chwords=Base32Encode(chwords);
+					if(chwords!=null&&chwords.length()>0){
 						key="mfg4_"+tdate+"_ChineseSet";
 						redisserver.sadd(key, chwords);
 						key="mfg4_"+tdate+"_Zh_"+chwords;
@@ -100,34 +99,57 @@ public class Yunguan_G4JK_ChineseWordsCountToRedis extends BaseRichBolt {
 	
 	//自定义方法区间
 	/**
-	 * 将明文的触点签名，转化为MD5 32位数字与字母组成的16进制编码
+	 * 将明文的触点签名，转化为 Base 32位数字与字母组成的编码
 	 * @param sign 触点需求为account，timestamp，key组合成的签名
-	 * @return resSign 转化后的MD5 32位 16进制编码，小写
+	 * @return resSign 转化后的Base 32位 数字与字母组成的编码，小写
 	 */
-	private String md5str32(String chwd){
-		String resSign=null;
+	private String Base32Encode(String chwd){
+		String res=null;
+		String base32Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";  //Base32加密算法
 		try{
-			 // 生成一个MD5加密计算摘要
-	        MessageDigest md = MessageDigest.getInstance("MD5");
-	        // 计算md5函数
-	        md.update(chwd.getBytes());
-	        // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
-	        byte b[] = md.digest();
-	        int i;
-	        StringBuffer buf = new StringBuffer("");
-	        for (int offset = 0; offset < b.length; offset++) {
-		        i = b[offset];
-		        if (i < 0)i += 256;
-		        if (i < 16)buf.append("0");
-		        buf.append(Integer.toHexString(i));
-	        }
-	        //最后获取32位的md5码
-	        resSign = buf.toString();
+	        byte bytes[] = chwd.getBytes();//获取中文转成的bytes
+	        int i = 0, index = 0, digit = 0;  
+	        int currByte, nextByte;  
+	        StringBuffer base32 = new StringBuffer((bytes.length + 7) * 8 / 5);  
+	        while (i < bytes.length) {  
+	            currByte = (bytes[i] >= 0) ? bytes[i] : (bytes[i] + 256); // 无符号  
+	            /* Is the current digit going to span a byte boundary? */  
+	            if (index > 3) {  
+	                if ((i + 1) < bytes.length) {  
+	                    nextByte = (bytes[i + 1] >= 0)?bytes[i + 1]:(bytes[i + 1] + 256);  
+	                } else {  
+	                    nextByte = 0;  
+	                }  
+	                digit = currByte & (0xFF >> index);  
+	                index = (index + 5) % 8;  
+	                digit <<= index;  
+	                digit |= nextByte >> (8 - index);  
+	                i++;  
+	            } else {  
+	                digit = (currByte >> (8 - (index + 5))) & 0x1F;  
+	                index = (index + 5) % 8;  
+	                if (index == 0) i++;  
+	            }  
+	            base32.append(base32Chars.charAt(digit));  
+	        }  
+	        res=base32.toString();
 		}catch(Exception ex){
-			LOG.info(" Thread md5str16 crashes: "+ex.getMessage());
+			LOG.info(" Thread Base32Encode crashes: "+ex.getMessage());
 			return null;
 		}
-		return resSign;
+		return res;
 	}
-
 }
+
+//private static final String base32Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";  
+//private static final int[] base32Lookup = { 0xFF, 0xFF, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, // '0', '1', '2', '3', '4', '5', '6', '7'  
+//      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // '8', '9', ':', ';', '<', '=',  '>', '?'  
+//      0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G'  
+//      0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, // 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'  
+//      0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, // 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'  
+//      0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // 'X', 'Y', 'Z', '[', '', ']', '^', '_'  
+//      0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g'  
+//      0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, // 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'  
+//      0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, // 'p', 'q', 'r', 's', 't', 'u', 'v', 'w'  
+//      0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF // 'x', 'y', 'z', '{', '|', '}', '~', 'DEL'  
+//}; 
