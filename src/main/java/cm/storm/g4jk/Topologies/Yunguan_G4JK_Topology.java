1f5cd742cc4d5ead89748fc2baf3f800276d4e33
chinamobile <yanxuexu007@126.com>
2017-03-03 10:53:32 +0800

    90

--- src/main/java/cm/storm/g4jk/Topologies/Yunguan_G4JK_Topology.java
+++ src/main/java/cm/storm/g4jk/Topologies/Yunguan_G4JK_Topology.java
@@ -20,7 +20,7 @@ import cm.storm.g4jk.Bolts.Yunguan_G4JK_ZhWordsToCountBolt;
 //import cm.storm.g4jk.Bolts.Yunguan_G4JK_BasicJKSplitter;
 import cm.storm.g4jk.Bolts.Yunguan_G4JK_HmapAccToRedis;
 import cm.storm.g4jk.Bolts.Yunguan_G4JK_HspAccToRedis;
-//import cm.storm.g4jk.Bolts.Yunguan_G4JK_SJJS093_93ToRedis;
+import cm.storm.g4jk.Bolts.Yunguan_G4JK_SJJS093_93ToRedis;
 //import cm.storm.g4jk.Bolts.Yunguan_G4JK_SJJS208_88ToRedis;
 //import cm.storm.g4jk.Bolts.Yunguan_G4JK_SJJS245_87ToRedis;
 import cm.storm.g4jk.Bolts.Yunguan_G4JK_TauAccToRedis;
@@ -105,10 +105,10 @@ public class Yunguan_G4JK_Topology {
         //统计热点区域人流量，标签，上网标签人数，热点区域流量数据，15分钟累计一次，将数据直接入库redis
         //基站周边人数，流量累计，15分钟累计一次，将数据直接入库redis
         //统计TAU中的人流量信息补充，15分钟累计一次，将数据直接入库redis
-        Tpbuilder.setBolt("HspAccBoltwfg4", new Yunguan_G4JK_HspAccToRedis(),20).shuffleGrouping("SplitterBoltwf4g");
-        Tpbuilder.setBolt("HmapAccBoltwfg4", new Yunguan_G4JK_HmapAccToRedis(),26).shuffleGrouping("SplitterBoltwf4g");
+        Tpbuilder.setBolt("HspAccBoltwfg4", new Yunguan_G4JK_HspAccToRedis(),22).shuffleGrouping("SplitterBoltwf4g");
+        Tpbuilder.setBolt("HmapAccBoltwfg4", new Yunguan_G4JK_HmapAccToRedis(),30).shuffleGrouping("SplitterBoltwf4g");
         Tpbuilder.setBolt("TauAccBoltwfg4", new Yunguan_G4JK_TauAccToRedis(),6).shuffleGrouping("SplitterBoltwftau");
-//        Tpbuilder.setBolt("SJJS093Bolt93wfg4", new Yunguan_G4JK_SJJS093_93ToRedis(),12).shuffleGrouping("SplitterBoltwf4g");
+        Tpbuilder.setBolt("SJJS093Bolt93wfg4", new Yunguan_G4JK_SJJS093_93ToRedis(),12).shuffleGrouping("SplitterBoltwf4g");
 //        Tpbuilder.setBolt("SJJS208Bolt88wfg4", new Yunguan_G4JK_SJJS208_88ToRedis(),12).shuffleGrouping("SJJS093Bolt93wfg4");
         Tpbuilder.setBolt("ZhToCountBoltwfg4", new Yunguan_G4JK_ZhWordsToCountBolt(),6).shuffleGrouping("SplitterBoltwf4g");
         Tpbuilder.setBolt("ZhCountBoltwfg4", new Yunguan_G4JK_ZhWordsCountToRedis(),6).shuffleGrouping("ZhToCountBoltwfg4");
